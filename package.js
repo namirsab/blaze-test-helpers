38a71cd25136f3d5fdd70b456ec828472e79e0fc
Namir <namirsab@gmail.com>
2017-01-26 11:54:04 +0100

    Update versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'planefy:blaze-test-helpers',
-  version: '0.0.2',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'Test Blaze templates',
   // URL to the Git repository containing the source code for this package.
